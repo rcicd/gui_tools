7381da71da6c68336ae1f4c88916f3f6f367e10f
Ilia Nechaev <lelikk2002@gmail.com>
2025-03-14 21:47:40 +0200

    Migrated to humble

--- assets/intrinsic_calibration_tool.sh
+++ assets/intrinsic_calibration_tool.sh
@@ -6,7 +6,7 @@ then
   echo "Need robot name"
   exit 1
 fi
-source /opt/ros/iron/setup.bash
+source /opt/$ROS_DISTRO/iron/setup.bash
 source ./install/setup.bash
 
 ros2 launch intrinsic_calibration_tool intrinsic_calibration_tool.launch vehicle:="$1"
--- assets/virtual_joystick.sh
+++ assets/virtual_joystick.sh
@@ -6,7 +6,7 @@ then
   echo "Need robot name"
   exit 1
 fi
-source /opt/ros/iron/setup.bash
+source /opt/ros/$ROS_DISTRO/setup.bash
 source ./install/setup.bash
 
 ros2 launch virtual_joystick virtual_joystick.launch vehicle:="$1"
